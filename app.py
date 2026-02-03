fb63cafade5b5d0db16ede3baf874e00aa008c84
persewoss-cell <persewoss@gmail.com>
2026-02-04 00:30:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+import streamlit as st
+
+if not firebase_admin._apps:
+    firebase_dict = json.loads(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
+    cred = credentials.Certificate(firebase_dict)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+st.title("🔥 Class Bank Connected to Firebase")
+
+
 import streamlit as st
 
 st.title("Class Bank is starting...")
