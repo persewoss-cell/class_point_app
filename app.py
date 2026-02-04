4bc0eb87228d0818ad1bdd5d1d088941048eff31
persewoss-cell <persewoss@gmail.com>
2026-02-04 14:16:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import json
-import re
-
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, firestore
-
+import pandas as pd
 from datetime import datetime, timezone, timedelta, date
 
-
+import firebase_admin
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 
 # (학급 확장용) PDF 텍스트 파싱(간단)
 import re
