9b52f0b445ac3f846b9a7cbbcf60891b171d3869
persewoss-cell <persewoss@gmail.com>
2026-02-05 15:49:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2163,14 +2163,14 @@ if "💼 직업/월급" in tabs:
             justify-content:center !important;
             color: #111 !important;
             overflow: hidden !important;
-            font-size: 0.78rem !important;
+            font-size: 0.47rem !important;
             font-weight: 900 !important;
         }
 
         /* 버튼 내부 모든 요소 축소 */
         .jobcnt-wrap div[data-testid="stButton"] button *,
         .joborder-wrap div[data-testid="stButton"] button *{
-            font-size: 0.78rem !important;
+            font-size: 0.47rem !important;
             font-weight: 900 !important;
             line-height: 1 !important;
             margin: 0 !important;
