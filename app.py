5d49eb14e7494f0e7368c483c06a0a3bba911f10
persewoss-cell <persewoss@gmail.com>
2026-03-09 14:45:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from supabase import create_client
+import os
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit.errors import StreamlitSecretNotFoundError
